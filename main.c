afd502d6d7df3661e66150f84b030678636f218a
Mattan Tseng <mattan.tseng@gmail.com>
2023-11-18 09:50:41 -0600

    Added timekeeping to inference

--- main.c
+++ main.c
@@ -18,14 +18,19 @@ int main() {
 	// network_train_batch_imgs(net, imgs, number_imgs);
 	// network_save(net, "testing_net");
 
-	// PREDICTING
-	// int number_imgs = 3000;
-	// Img** imgs = csv_to_imgs("data/mnist_test.csv", number_imgs);
-	// NeuralNetwork* net = network_load("testing_net");
-	// double score = network_predict_imgs(net, imgs, 1000);
-	// printf("Score: %1.5f\n", score);
+	time_t startTime = clock(); 
+	int number_imgs = 3000;
+	Img** imgs = csv_to_imgs("data/mnist_test.csv", number_imgs);
+	NeuralNetwork* net = network_load("testing_net");
+	double score = network_predict_imgs(net, imgs, 1000);
+	printf("Score: %1.5f\n", score);
 
-	// imgs_free(imgs, number_imgs);
-	// network_free(net);
+	imgs_free(imgs, number_imgs);
+	network_free(net);
+
+	time_t endTime = clock();
+
+	double inferenceTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+	printf("Inference time: %f \n", inferenceTime);
 	return 0;
 }
