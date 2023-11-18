0b8146e4625a34fa42d59de0097041bbc42f8645
Mattan Tseng <mattan.tseng@gmail.com>
2023-11-18 10:48:43 -0600

    inference

--- main.c
+++ main.c
@@ -9,51 +9,51 @@
 #include "matrix/ops.h"
 
 int main() {
-	srand(time(NULL));
-	//TRAINING
-	printf("Starting training\n");
-	// record the start time
-	time_t startTime = time(NULL);
+	// srand(time(NULL));
+	// //TRAINING
+	// printf("Starting training\n");
+	// // record the start time
+	// time_t startTime = time(NULL);
 
-	int number_imgs = 10000;
-	time_t startLoadTime = time(NULL);
-	Img** imgs = csv_to_imgs("./data/mnist_test.csv", number_imgs);
-	time_t doneLoadTime = time(NULL);
+	// int number_imgs = 10000;
+	// time_t startLoadTime = time(NULL);
+	// Img** imgs = csv_to_imgs("./data/mnist_test.csv", number_imgs);
+	// time_t doneLoadTime = time(NULL);
 
 
-	NeuralNetwork* net = network_create(784, 1000, 10, 0.1);
+	// NeuralNetwork* net = network_create(784, 1000, 10, 0.1);
 	
-	time_t startTrainTime = time(NULL);
-	network_train_batch_imgs(net, imgs, number_imgs);
-	time_t doneTrainTime = time(NULL);
+	// time_t startTrainTime = time(NULL);
+	// network_train_batch_imgs(net, imgs, number_imgs);
+	// time_t doneTrainTime = time(NULL);
 	
-	network_save(net, "testing_net");
-
-	time_t endTime = time(NULL);
-
-	double trainingTime = (double)(doneTrainTime - startTrainTime); // / CLOCKS_PER_SEC;
-	double imageLoadTime = (double)(doneLoadTime - startLoadTime); // / CLOCKS_PER_SEC;
-	double totalTime = (double)(endTime - startTime); // / CLOCKS_PER_SEC;
-
-
-	printf("Image load time: %f \n", imageLoadTime);
-	printf("Training time: %f \n", trainingTime);
-	printf("Total time: %f \n", totalTime);
-
-
-	// time_t startTime = time(NULL); 
-	// int number_imgs = 3000;
-	// Img** imgs = csv_to_imgs("data/mnist_test.csv", number_imgs);
-	// NeuralNetwork* net = network_load("testing_net");
-	// double score = network_predict_imgs(net, imgs, 1000);
-	// printf("Score: %1.5f\n", score);
-
-	// imgs_free(imgs, number_imgs);
-	// network_free(net);
+	// network_save(net, "testing_net");
 
 	// time_t endTime = time(NULL);
 
-	// double inferenceTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
-	// printf("Inference time: %f \n", inferenceTime);
+	// double trainingTime = (double)(doneTrainTime - startTrainTime); // / CLOCKS_PER_SEC;
+	// double imageLoadTime = (double)(doneLoadTime - startLoadTime); // / CLOCKS_PER_SEC;
+	// double totalTime = (double)(endTime - startTime); // / CLOCKS_PER_SEC;
+
+
+	// printf("Image load time: %f \n", imageLoadTime);
+	// printf("Training time: %f \n", trainingTime);
+	// printf("Total time: %f \n", totalTime);
+
+
+	time_t startTime = time(NULL); 
+	int number_imgs = 3000;
+	Img** imgs = csv_to_imgs("data/mnist_test.csv", number_imgs);
+	NeuralNetwork* net = network_load("testing_net");
+	double score = network_predict_imgs(net, imgs, 1000);
+	printf("Score: %1.5f\n", score);
+
+	imgs_free(imgs, number_imgs);
+	network_free(net);
+
+	time_t endTime = time(NULL);
+
+	double inferenceTime = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+	printf("Inference time: %f \n", inferenceTime);
 	return 0;
 }
