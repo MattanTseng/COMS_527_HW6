e6ff8a0cdd0db0074caacd25f7954683235f8061
Mattan Tseng <mattan.tseng@gmail.com>
2023-11-18 08:59:34 -0600

    made some formatting changes

--- matrix/ops.c
+++ matrix/ops.c
@@ -12,16 +12,14 @@ int check_dimensions(Matrix *m1, Matrix *m2) {
 Matrix* multiply(Matrix *m1, Matrix *m2) {
 	if (check_dimensions(m1, m2)) {
 		Matrix *m = matrix_create(m1->rows, m1->cols);
-		# pragma omp target{
-
+# 		pragma omp target
 #		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-		for (int i = 0; i < m1->rows; i++) {
-			for (int j = 0; j < m2->cols; j++) {
-				m->entries[i][j] = m1->entries[i][j] * m2->entries[i][j];
+			for (int i = 0; i < m1->rows; i++) {
+				for (int j = 0; j < m2->cols; j++) {
+					m->entries[i][j] = m1->entries[i][j] * m2->entries[i][j];
+				}
 			}
-		}
-		}
-		return m;
+			return m;
 	} else {
 		printf("Dimension mistmatch multiply: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
 		exit(1);
@@ -31,31 +29,28 @@ Matrix* multiply(Matrix *m1, Matrix *m2) {
 Matrix* add(Matrix *m1, Matrix *m2) {
 	if (check_dimensions(m1, m2)) {
 		Matrix *m = matrix_create(m1->rows, m1->cols);
-# 		pragma omp target{
-	#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-			for (int i = 0; i < m1->rows; i++) {
-				for (int j = 0; j < m2->cols; j++) {
-					m->entries[i][j] = m1->entries[i][j] + m2->entries[i][j];
-				}
+# 		pragma omp target
+#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+		for (int i = 0; i < m1->rows; i++) {
+			for (int j = 0; j < m2->cols; j++) {
+				m->entries[i][j] = m1->entries[i][j] + m2->entries[i][j];
 			}
-			return m;
-		} else {
-			printf("Dimension mistmatch add: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
-			exit(1);
 		}
+		return m;
+	} else {
+		printf("Dimension mistmatch add: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
+		exit(1);
 	}
 }
 
 Matrix* subtract(Matrix *m1, Matrix *m2) {
 	if (check_dimensions(m1, m2)) {
 		Matrix *m = matrix_create(m1->rows, m1->cols);
-		# pragma omp target{
-
-	#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-			for (int i = 0; i < m1->rows; i++) {
-				for (int j = 0; j < m2->cols; j++) {
-					m->entries[i][j] = m1->entries[i][j] - m2->entries[i][j];
-				}
+# 		pragma omp target
+#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+		for (int i = 0; i < m1->rows; i++) {
+			for (int j = 0; j < m2->cols; j++) {
+				m->entries[i][j] = m1->entries[i][j] - m2->entries[i][j];
 			}
 		}
 		return m;
@@ -67,13 +62,11 @@ Matrix* subtract(Matrix *m1, Matrix *m2) {
 
 Matrix* apply(double (*func)(double), Matrix* m) {
 	Matrix *mat = matrix_copy(m);
-	# pragma omp target{
-
-	#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-		for (int i = 0; i < m->rows; i++) {
-			for (int j = 0; j < m->cols; j++) {
-				mat->entries[i][j] = (*func)(m->entries[i][j]);
-			}
+#	pragma omp target
+#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			mat->entries[i][j] = (*func)(m->entries[i][j]);
 		}
 	}
 	return mat;
@@ -82,16 +75,15 @@ Matrix* apply(double (*func)(double), Matrix* m) {
 Matrix* dot(Matrix *m1, Matrix *m2) {
 	if (m1->cols == m2->rows) {
 		Matrix *m = matrix_create(m1->rows, m2->cols);
-		# pragma omp target{
-	#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-			for (int i = 0; i < m1->rows; i++) {
-				for (int j = 0; j < m2->cols; j++) {
-					double sum = 0;
-					for (int k = 0; k < m2->rows; k++) {
-						sum += m1->entries[i][k] * m2->entries[k][j];
-					}
-					m->entries[i][j] = sum;
+#		pragma omp target
+#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+		for (int i = 0; i < m1->rows; i++) {
+			for (int j = 0; j < m2->cols; j++) {
+				double sum = 0;
+				for (int k = 0; k < m2->rows; k++) {
+					sum += m1->entries[i][k] * m2->entries[k][j];
 				}
+				m->entries[i][j] = sum;
 			}
 		}
 		return m;
@@ -103,13 +95,11 @@ Matrix* dot(Matrix *m1, Matrix *m2) {
 
 Matrix* scale(double n, Matrix* m) {
 	Matrix* mat = matrix_copy(m);
-	# pragma omp target{
-
-	#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-		for (int i = 0; i < m->rows; i++) {
-			for (int j = 0; j < m->cols; j++) {
-				mat->entries[i][j] *= n;
-			}
+# 	pragma omp target
+#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			mat->entries[i][j] *= n;
 		}
 	}
 	return mat;
@@ -117,26 +107,23 @@ Matrix* scale(double n, Matrix* m) {
 
 Matrix* addScalar(double n, Matrix* m) {
 	Matrix* mat = matrix_copy(m);
-	# pragma omp target{
-
-	#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+# 	pragma omp target
+#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
 		for (int i = 0; i < m->rows; i++) {
 			for (int j = 0; j < m->cols; j++) {
 				mat->entries[i][j] += n;
 			}
 		}
-	}
 	return mat;
 }
 
 Matrix* transpose(Matrix* m) {
 	Matrix* mat = matrix_create(m->cols, m->rows);
-# pragma omp target{
-	#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-		for (int i = 0; i < m->rows; i++) {
-			for (int j = 0; j < m->cols; j++) {
-				mat->entries[j][i] = m->entries[i][j];
-			}
+# pragma omp target
+#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+	for (int i = 0; i < m->rows; i++) {
+		for (int j = 0; j < m->cols; j++) {
+			mat->entries[j][i] = m->entries[i][j];
 		}
 	}
 	return mat;
