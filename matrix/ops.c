f1c2be0d831a2e05680b5f264a29350b08c295e8
Mattan Tseng <mattan.tseng@gmail.com>
2023-11-24 10:47:13 -0600

    Adding updates from piazza

--- matrix/ops.c
+++ matrix/ops.c
@@ -8,56 +8,103 @@ int check_dimensions(Matrix *m1, Matrix *m2) {
 	if (m1->rows == m2->rows && m1->cols == m2->cols) return 1;
 	return 0;
 }
-
 Matrix* multiply(Matrix *m1, Matrix *m2) {
-	if (check_dimensions(m1, m2)) {
-		Matrix *m = matrix_create(m1->rows, m1->cols);
-# 		pragma omp target
-#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-			for (int i = 0; i < m1->rows; i++) {
-				for (int j = 0; j < m2->cols; j++) {
-					m->entries[i][j] = m1->entries[i][j] * m2->entries[i][j];
-				}
-			}
-			return m;
-	} else {
-		printf("Dimension mistmatch multiply: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
-		exit(1);
-	}
+    if (check_dimensions(m1, m2)) {
+        Matrix *m = matrix_create(m1->rows, m1->cols);
+
+        // Temporary pointers for the data mapping
+        double **m1_entries = m1->entries;
+        double **m2_entries = m2->entries;
+        double **m_entries = m->entries;
+
+        // Map the data to the device (GPU)
+        #pragma omp target enter data map(to: m1[:1], m2[:1])
+        #pragma omp target enter data map(to: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+        #pragma omp target enter data map(alloc: m_entries[:m->rows * m->cols])
+
+        // Perform matrix multiplication on the GPU
+        #pragma omp target teams distribute parallel for collapse(2) thread_limit(NUM_THREADS)
+        for (int i = 0; i < m1->rows; i++) {
+            for (int j = 0; j < m2->cols; j++) {
+                m_entries[i][j] = m1_entries[i][j] * m2_entries[i][j];
+            }
+        }
+
+        // Retrieve the results from the device (you can merge this codes to above map to as well)
+        #pragma omp target exit data map(from: m_entries[:m->rows * m->cols])
+        #pragma omp target exit data map(delete: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+
+        return m;
+    } else {
+        printf("Dimension mismatch multiply: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
+        exit(1);
+    }
 }
 
 Matrix* add(Matrix *m1, Matrix *m2) {
-	if (check_dimensions(m1, m2)) {
-		Matrix *m = matrix_create(m1->rows, m1->cols);
-# 		pragma omp target
-#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-		for (int i = 0; i < m1->rows; i++) {
-			for (int j = 0; j < m2->cols; j++) {
-				m->entries[i][j] = m1->entries[i][j] + m2->entries[i][j];
-			}
-		}
-		return m;
-	} else {
-		printf("Dimension mistmatch add: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
-		exit(1);
-	}
+    if (check_dimensions(m1, m2)) {
+        Matrix *m = matrix_create(m1->rows, m1->cols);
+
+        // Temporary pointers for the data mapping
+        double **m1_entries = m1->entries;
+        double **m2_entries = m2->entries;
+        double **m_entries = m->entries;
+
+        // Map the data to the device (GPU)
+        #pragma omp target enter data map(to: m1[:1], m2[:1])
+        #pragma omp target enter data map(to: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+        #pragma omp target enter data map(alloc: m_entries[:m->rows * m->cols])
+
+        // Perform matrix multiplication on the GPU
+        #pragma omp target teams distribute parallel for collapse(2) thread_limit(NUM_THREADS)
+        for (int i = 0; i < m1->rows; i++) {
+            for (int j = 0; j < m2->cols; j++) {
+                m_entries[i][j] = m1_entries[i][j] + m2_entries[i][j];
+            }
+        }
+
+        // Retrieve the results from the device (you can merge this codes to above map to as well)
+        #pragma omp target exit data map(from: m_entries[:m->rows * m->cols])
+        #pragma omp target exit data map(delete: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+
+        return m;
+    } else {
+        printf("Dimension mismatch multiply: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
+        exit(1);
+    }
 }
 
 Matrix* subtract(Matrix *m1, Matrix *m2) {
-	if (check_dimensions(m1, m2)) {
-		Matrix *m = matrix_create(m1->rows, m1->cols);
-# 		pragma omp target
-#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
-		for (int i = 0; i < m1->rows; i++) {
-			for (int j = 0; j < m2->cols; j++) {
-				m->entries[i][j] = m1->entries[i][j] - m2->entries[i][j];
-			}
-		}
-		return m;
-	} else {
-		printf("Dimension mistmatch subtract: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
-		exit(1);
-	}
+    if (check_dimensions(m1, m2)) {
+        Matrix *m = matrix_create(m1->rows, m1->cols);
+
+        // Temporary pointers for the data mapping
+        double **m1_entries = m1->entries;
+        double **m2_entries = m2->entries;
+        double **m_entries = m->entries;
+
+        // Map the data to the device (GPU)
+        #pragma omp target enter data map(to: m1[:1], m2[:1])
+        #pragma omp target enter data map(to: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+        #pragma omp target enter data map(alloc: m_entries[:m->rows * m->cols])
+
+        // Perform matrix multiplication on the GPU
+        #pragma omp target teams distribute parallel for collapse(2) thread_limit(NUM_THREADS)
+        for (int i = 0; i < m1->rows; i++) {
+            for (int j = 0; j < m2->cols; j++) {
+                m_entries[i][j] = m1_entries[i][j] - m2_entries[i][j];
+            }
+        }
+
+        // Retrieve the results from the device (you can merge this codes to above map to as well)
+        #pragma omp target exit data map(from: m_entries[:m->rows * m->cols])
+        #pragma omp target exit data map(delete: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+
+        return m;
+    } else {
+        printf("Dimension mismatch multiply: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
+        exit(1);
+    }
 }
 
 Matrix* apply(double (*func)(double), Matrix* m) {
