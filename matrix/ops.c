d47229fc9f3514402ba5264ddf29347db43523bf
Mattan Tseng <mattan.tseng@gmail.com>
2023-11-24 11:37:01 -0600

    still implementing piazza updates

--- matrix/ops.c
+++ matrix/ops.c
@@ -109,21 +109,39 @@ Matrix* subtract(Matrix *m1, Matrix *m2) {
 
 Matrix* apply(double (*func)(double), Matrix* m) {
 	Matrix *mat = matrix_copy(m);
-#	pragma omp target
-#	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+
+	double **mat_entries = mat->entries;
+	#pragma omp target enter data map(alloc: mat_entries[:mat->rows * mat->cols])
+	#pragma omp target teams distribute parallel for collapse(2) thread_limit(NUM_THREADS)
 	for (int i = 0; i < m->rows; i++) {
 		for (int j = 0; j < m->cols; j++) {
-			mat->entries[i][j] = (*func)(m->entries[i][j]);
+			mat->entries[i][j] = (*func)(mat_entries[i][j]);
 		}
 	}
 	return mat;
 }
 
+
+
+
+
+
 Matrix* dot(Matrix *m1, Matrix *m2) {
 	if (m1->cols == m2->rows) {
 		Matrix *m = matrix_create(m1->rows, m2->cols);
-#		pragma omp target
-#		pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
+
+        // Temporary pointers for the data mapping
+        double **m1_entries = m1->entries;
+        double **m2_entries = m2->entries;
+        double **m_entries = m->entries;
+
+ // Map the data to the device (GPU)
+        #pragma omp target enter data map(to: m1[:1], m2[:1])
+        #pragma omp target enter data map(to: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+        #pragma omp target enter data map(alloc: m_entries[:m->rows * m->cols])
+
+        // Perform matrix multiplication on the GPU
+        #pragma omp target teams distribute parallel for collapse(2) thread_limit(NUM_THREADS)
 		for (int i = 0; i < m1->rows; i++) {
 			for (int j = 0; j < m2->cols; j++) {
 				double sum = 0;
@@ -133,6 +151,11 @@ Matrix* dot(Matrix *m1, Matrix *m2) {
 				m->entries[i][j] = sum;
 			}
 		}
+
+		        // Retrieve the results from the device (you can merge this codes to above map to as well)
+        #pragma omp target exit data map(from: m_entries[:m->rows * m->cols])
+        #pragma omp target exit data map(delete: m1_entries[:m1->rows * m1->cols], m2_entries[:m2->rows * m2->cols])
+		
 		return m;
 	} else {
 		printf("Dimension mistmatch dot: %dx%d %dx%d\n", m1->rows, m1->cols, m2->rows, m2->cols);
@@ -154,6 +177,7 @@ Matrix* scale(double n, Matrix* m) {
 
 Matrix* addScalar(double n, Matrix* m) {
 	Matrix* mat = matrix_copy(m);
+
 # 	pragma omp target
 #	pragma omp parallel for num_threads(NUM_THREADS) collapse(2)
 		for (int i = 0; i < m->rows; i++) {
